a2d0bb3f0fdcf8d939815e77c4b5b6913a7fe320
Jeff Kim <jeffkangkim@gmail.com>
2016-01-22 14:51:19 -0600

    Added Healthbars to all subclasses of TemplateClass.

--- HighSens/src/FastMonster.java
+++ HighSens/src/FastMonster.java
@@ -3,218 +3,223 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.geom.Point2D;
 import java.io.File;
+
 import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
 
-public class FastMonster extends TemplateClass implements Monster, GameFigure, IStrategy{
-    
-    Image fastMonsterImage;
-    Sound deathSound;
-    Point2D.Float target;
-    Missile missile;
-    GameData gd;
-    private int state;
-    public float x, y;
-    int health = 100;
-    int speed = 8;
-    int buffCount = 0;
+public class FastMonster extends TemplateClass implements Monster, GameFigure, IStrategy {
 
-    public FastMonster(float x, float y, GameData gd) {
-        this.gd = gd;
-        this.state = PATH_0;
-        this.x = x;
-        this.y = y;
-        String imagePath = System.getProperty("user.dir");
-        String separator = System.getProperty("file.separator");
-        fastMonsterImage = getImage(imagePath + separator + "images" + separator
-                + "fastMonster.png");
-    }
-    
-    @Override
-    public Image getImage(String fileName) {
-        Image image = null;
-        try {
-            image = ImageIO.read(new File(fileName));
-        } catch (Exception ioe) {
-            System.out.println("Error: Cannot open image:" + fileName);
-            JOptionPane.showMessageDialog(null, "Error: Cannot open image:" + fileName);
-        }
-        return image;
-    }
+	Image fastMonsterImage;
+	Sound deathSound;
+	Point2D.Float target;
+	Missile missile;
+	GameData gd;
+	private int state;
+	public float x, y;
 
-    @Override
-    public void render(Graphics g) {
-        g.drawImage(fastMonsterImage, (int)x, (int)y, null);
-    }
+	int buffCount = 0;
 
-    @Override
-    public void update() {
-        updateState();
-        switch(state)
-        {
-            case PATH_0:
-                moveRight();
-                break;
-            case PATH_1:
-                moveUp();
-                break;
-            case PATH_2:
-                moveRight();
-                break;
-            case PATH_3:
-                moveDown();
-                break;
-            case PATH_4:
-                moveRight();
-                break;
-            case PATH_5:
-                moveUp();
-                break;
-            case PATH_6:
-                moveRight();
-                break;
-            case STATE_DONE:
-                deathSound = new Sound("sounds/pop.wav");
-                deathSound.start();
-                gd.moneyManager("kill2", gd.getMoney());
-                break;
-            case LIFE_LOST:
-                updateLives();
-                state = STATE_DONE;
-                break;
-        }
-    }
-    
-    @Override
-    public void updateState() {
-        switch(state)
-        {
-            case PATH_0:
-                if(x >= 69) state = PATH_1;
-                break;
-            case PATH_1:
-                if(y <= 87) state = PATH_2;
-                break;
-            case PATH_2:
-                if(x >= 200) state = PATH_3;                        
-                break;
-            case PATH_3:
-                if(y >= 240) state = PATH_4;
-                break;
-            case PATH_4:
-                if(x >= 350) state = PATH_5;
-                break;
-            case PATH_5:
-                if(y <= 160) state = PATH_6;
-                break;  
-            case PATH_6:
-                if(x >= 600) state = LIFE_LOST;
-        }
-    }
+	public FastMonster(float x, float y, GameData gd) {
+		this.gd = gd;
+		this.state = PATH_0;
+		this.x = x;
+		this.y = y;
+		this.maxHealth = 100;
+		this.health = maxHealth;
+		this.speed = 8;
+		String imagePath = System.getProperty("user.dir");
+		String separator = System.getProperty("file.separator");
+		fastMonsterImage = getImage(imagePath + separator + "images" + separator + "fastMonster.png");
+	}
 
-    public void moveRight() {
-       x+=speed;
-    }
-    public void moveLeft() {
-        x-=speed;
-    }
-    public void moveUp() {
-        y-=speed;
-    }
-    public void moveDown() {
-        y+=speed;
-    }
-    
-    @Override
-    public int getState() {
-        return state;
-        
-    }
+	@Override
+	public Image getImage(String fileName) {
+		Image image = null;
+		try {
+			image = ImageIO.read(new File(fileName));
+		} catch (Exception ioe) {
+			System.out.println("Error: Cannot open image:" + fileName);
+			JOptionPane.showMessageDialog(null, "Error: Cannot open image:" + fileName);
+		}
+		return image;
+	}
 
-    public void setHealth(int health)
-    {
-        this.health = health;
-    }
-    
-    public int getHealth() {
-        return health;
-    }
-    
-    @Override
-    public void setState(int state) {
-        this.state = state;
-    }
+	@Override
+	public void render(Graphics g) {
+		drawHealthBar(g, x, y);
+		g.drawImage(fastMonsterImage, (int) x, (int) y, null);
+	}
 
-    @Override
-    public double getX() {
-        return (float)x;
-        
-    }
+	@Override
+	public void update() {
+		updateState();
+		switch (state) {
+		case PATH_0:
+			moveRight();
+			break;
+		case PATH_1:
+			moveUp();
+			break;
+		case PATH_2:
+			moveRight();
+			break;
+		case PATH_3:
+			moveDown();
+			break;
+		case PATH_4:
+			moveRight();
+			break;
+		case PATH_5:
+			moveUp();
+			break;
+		case PATH_6:
+			moveRight();
+			break;
+		case STATE_DONE:
+			deathSound = new Sound("sounds/pop.wav");
+			deathSound.start();
+			gd.moneyManager("kill2", gd.getMoney());
+			break;
+		case LIFE_LOST:
+			updateLives();
+			state = STATE_DONE;
+			break;
+		}
+	}
 
-    @Override
-    public double getY() {
-        return (float)y;
-        
-    }
+	@Override
+	public void updateState() {
+		switch (state) {
+		case PATH_0:
+			if (x >= 69)
+				state = PATH_1;
+			break;
+		case PATH_1:
+			if (y <= 87)
+				state = PATH_2;
+			break;
+		case PATH_2:
+			if (x >= 200)
+				state = PATH_3;
+			break;
+		case PATH_3:
+			if (y >= 240)
+				state = PATH_4;
+			break;
+		case PATH_4:
+			if (x >= 350)
+				state = PATH_5;
+			break;
+		case PATH_5:
+			if (y <= 160)
+				state = PATH_6;
+			break;
+		case PATH_6:
+			if (x >= 600)
+				state = LIFE_LOST;
+		}
+	}
 
-    public boolean contains(float x, float y)
-    {
-       if(x < this.x)
-          return false;
-       if(x > this.x + 47)
-           return false;
-       if(y < this.y)
-           return false;
-       if(y > this.y + 37)
-           return false;
-       
-       return true;
-    }
-    
-    @Override
-    public void updateHealth() {
-        health -= 5;
-        if(health <= 0){
-            state = STATE_DONE;
-        } else if ((health >= (health/4) && (health <= (health/4) + 5)))
-        {
-            if(buffCount < 1)
-            {
-               getAngry();
-            }
-            buffCount++;
-        }
-    }
+	public void moveRight() {
+		x += speed;
+	}
 
-    public void getAngry()
-    {
-       health += 50;
-       speed += 4;
-    }
-    
-    public int getScore()
-    {
-        return gd.score += 10;
-    }
-    
-    @Override
-    public boolean collision(GameFigure m) {
-        return false;
-        
-    }   
+	public void moveLeft() {
+		x -= speed;
+	}
 
-    @Override
-    public void updateLives() {
-        gd.minusLives();
-    }
+	public void moveUp() {
+		y -= speed;
+	}
 
-    @Override
-    public void setX(float x) {
-        this.x = x;
-    }
+	public void moveDown() {
+		y += speed;
+	}
 
-    @Override
-    public void setY(float y) {
-        this.y = y;
-    }
+	@Override
+	public int getState() {
+		return state;
+
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	@Override
+	public void setState(int state) {
+		this.state = state;
+	}
+
+	@Override
+	public double getX() {
+		return (float) x;
+
+	}
+
+	@Override
+	public double getY() {
+		return (float) y;
+
+	}
+
+	public boolean contains(float x, float y) {
+		if (x < this.x)
+			return false;
+		if (x > this.x + 47)
+			return false;
+		if (y < this.y)
+			return false;
+		if (y > this.y + 37)
+			return false;
+
+		return true;
+	}
+
+	@Override
+	public void updateHealth() {
+		health -= 5;
+		if (health <= 0) {
+			state = STATE_DONE;
+		} else if ((health >= (health / 4) && (health <= (health / 4) + 5))) {
+			if (buffCount < 1) {
+				getAngry();
+			}
+			buffCount++;
+		}
+	}
+
+	public void getAngry() {
+		health += 50;
+		speed += 4;
+	}
+
+	public int getScore() {
+		return gd.score += 10;
+	}
+
+	@Override
+	public boolean collision(GameFigure m) {
+		return false;
+
+	}
+
+	@Override
+	public void updateLives() {
+		gd.minusLives();
+	}
+
+	@Override
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	@Override
+	public void setY(float y) {
+		this.y = y;
+	}
 }
--- HighSens/src/TemplateClass.java
+++ HighSens/src/TemplateClass.java
@@ -1,12 +1,36 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public abstract class TemplateClass {
 
-    int health;
-    int speed;
+	protected int maxHealth;
+	protected int speed;
+	protected int health; 
+	public TemplateClass() {
+	}
 
-    public void getAngry()
-    {
-        this.health = health;
-        this.speed = speed;
-    }
- }
+	/**
+	 * Draws healthbar on x and y coordinates.
+	 */
+	protected void drawHealthBar(Graphics g, float x, float y) {
+		Graphics2D healthBar = (Graphics2D) g;
+		healthBar.setColor(Color.RED);
+		healthBar.setFont(new Font("Serif", Font.PLAIN, 10));
+		// healthBar.drawString("hp:" + health, x - 5, y - 5);
+		healthBar.drawString(calculateHealthbarSize(), x - 5, y - 5);
+	}
+
+	private String calculateHealthbarSize() {
+		String healthBar = "";
+		int currentHealth = health;
+		while (currentHealth - (maxHealth / 10) > 1) {
+			currentHealth = currentHealth - (maxHealth / 10);
+			healthBar += "=";
+		}
+
+		return healthBar;
+	}
+
+}
