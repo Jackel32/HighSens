acb1465edb89466ed0c581012b9a8f2de3a2d117
travislmeyers <tmeyers@uco.edu>
2016-01-31 14:40:30 -0600

    Nothing to add. Just commiting so I can sync.

---
+++ HighSens/src/TemplateClass.java
@@ -0,0 +1,12 @@
+
+public abstract class TemplateClass {
+
+    int health;
+    int speed;
+
+    public void getAngry()
+    {
+        this.health = health;
+        this.speed = speed;
+    }
+ }
