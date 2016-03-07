95e41ed793cd94703b41a6d562422481de8735e7
Matt Kincheloe <matt.kinch@gmail.com>
2016-03-06 20:17:21 -0600

    Store updates

---
+++ HighSens/src/com/highsens/game/Player.java
@@ -0,0 +1,25 @@
+package com.highsens.game;
+
+public class Player{
+	private static Player instance = null;
+	private static int gameCash = 1000;
+	
+	protected Player(){
+	      // Exists only to defeat instantiation.
+	}
+	
+	public static Player getInstance(){
+		if(instance == null){
+			instance = new Player();
+	    }
+		return instance;
+	}
+	
+	public static int getGameCash(){
+		return gameCash;
+	}
+	
+	public static void setGameCash(int newCash){
+		gameCash = newCash;
+	}
+}
